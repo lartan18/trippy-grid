d3cb973ed085e072e3e0d9b44ddd0be1e7de4304
Lars Tangen-Hestnes <larstang@stud.ntnu.no>
2025-01-17 10:48:07 +0100

    looks better now

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const container = document.querySelector("#container")
 const size = 20
 container.innerHTML = ""
 
-for (let x = 0; x < size; x++) {
-    for (let y = 0; y < size; y++) {
+for (let x = -size/2; x < size / 2; x++) {
+    for (let y = -size/2; y < size / 2; y++) {
         container.innerHTML += `<div class="tile" data-x=${x} data-y=${y}></div>` // <p>${x}</p><p>${y}</p>
     }
 }
@@ -12,7 +12,16 @@ const tiles = document.querySelectorAll(".tile")
 
 tiles.forEach(tile => {
     tile.addEventListener("mouseover", _ => {
-        container.style["transform"] = `rotateX(${90-50-tile.dataset.x*Math.log2(tile.dataset.x)}deg) rotateY(${45-50+tile.dataset.y*Math.log10(tile.dataset.y)}deg) rotateZ(${-Math.abs(-Math.log2(tile.dataset.x))}deg) scale(${Math.log2(tile.dataset.x+tile.dataset.y)/10})  translate(-200px, ${- tile.dataset.y/10}px)`
+        const multiplier = 2
+        const x = tile.dataset.x
+        const y = tile.dataset.y
+        container.style["transform"] = `
+        rotateX(${-x*multiplier}deg)
+        rotateY(${y*multiplier}deg)
+        scale(0.5)
+        `
+        // ${0.5+Math.abs((x*y))/Math.abs(x+y)}
+        // container.style["transform"] = `rotateX(${90-50-tile.dataset.x*Math.log2(tile.dataset.x)}deg) rotateY(${45-50+tile.dataset.y*Math.log10(tile.dataset.y)}deg) rotateZ(${-Math.abs(-Math.log2(tile.dataset.x))}deg) scale(${Math.log2(tile.dataset.x+tile.dataset.y)/10})  translate(-200px, ${- tile.dataset.y/10}px)`
         console.log(container.style.rotate)
     })
 })
